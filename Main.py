68570076444662c125ec2872659bcbd18d576b7e
Felipe Villegas <91444302+Felipeortizvi@users.noreply.github.com>
2021-11-06 15:58:22 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -141,6 +141,11 @@ def main(window, totalWidth):
                     startTile = None
                 elif clickedTile == endTile:
                     endTile = None
+		
+	elif event.type == pygame.KEYDOWN:
+                clickedTile = getClickedTile(grid,WIDTH,ROWS)
+                if  event.key == pygame.K_SPACE:
+                    clickedTile.tileType = BARRIER_WATER
 
         draw(window, grid, totalWidth, COLS, ROWS)
 
